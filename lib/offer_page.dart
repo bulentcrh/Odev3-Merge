b1871faedee71157cad1783ea210f08e72dde33b
Bülent ÇORUH <bulentcoruh7@gmail.com>
2023-05-23 18:29:06 +0300

    new update

---
+++ lib/offer_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OfferPage extends StatefulWidget {
+  const OfferPage({super.key});
+
+  @override
+  State<OfferPage> createState() => _OfferPageState();
+}
+
+class _OfferPageState extends State<OfferPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
